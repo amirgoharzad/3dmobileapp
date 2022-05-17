302d27db5d936bf2de58df48d265381327579325
amirgoharzad <amirgoharzad@yahoo.com>
2022-05-17 18:21:21 +0430

    x3dom js url changed

--- application/view/models.php
+++ application/view/models.php
@@ -14,7 +14,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
             crossorigin="anonymous"></script>
-    <script type='text/javascript' src='http://www.x3dom.org/x3dom/release/x3dom.js'></script>
+    <script type='text/javascript' src='https://www.x3dom.org/x3dom/release/x3dom.js'></script>
     <script type="text/javascript" src="<?= base_url ?>application/js/createGallery.js"></script>
     <script src="https://code.jquery.com/jquery-latest.js"></script>
     <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
